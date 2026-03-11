328d2dee5e9892a05b132e82aad35040334bbd07
Aaron <aaronwan808@gmail.com>
2026-03-11 00:28:36 +0000

    adding IO monad and messing around

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,22 @@
-import java.io.File
+import cats.effect.{ExitCode, IO, IOApp}
+import fs2.Stream
+import fs2.io.file.{Files, Path}
 
-object Main {
+object Main extends IOApp {
 
-  def getListOfFilesFromDir(dir: String): List[String] = {
-    val file = new File(dir)
-    file.listFiles.filter(_.isFile)
-      .filter(_.getName.endsWith(".json"))
-      .map(_.getPath).toList
+  def findJsonFiles(dir: Path): Stream[IO, Path] =
+    Files[IO]
+      .walk(dir)
+      .filter(_.extName == ".json")
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    val dir = args.headOption.getOrElse(System.getProperty("user.dir"))
+    val path = Path(dir)
+
+    findJsonFiles(path)
+      .evalMap(p => IO.println(p.toString))
+      .compile
+      .drain
+      .as(ExitCode.Success)
   }
-
-  def main(args: Array[String]) = {
-    // need to read all directories recursively then look for json or whatever file format
-    val currentPath = System.getProperty("user.dir")
-    val listOfFiles = getListOfFilesFromDir(currentPath)
-    println(listOfFiles)
-  }
-
 }
