318fd44d6888ee04038be158a6f8c452bcb2c4f8
Aaron <aaronwan808@gmail.com>
2026-03-29 18:35:20 +0100

    working on task resolver

---
+++ src/main/scala/core/TaskResolver.scala
@@ -0,0 +1,48 @@
+package core
+import adapters.TaskSource.{Npm, Yaml}
+
+case class UnresolvedTask(task: Task, dependencyNames: List[String])
+
+object TaskManager {
+
+  def resolveAll(discoveredTasks: List[DiscoveredTask]): List[Task] = {
+    val unresolved = discoveredTasks.map(toUnresolvedTask)
+    val allTasks = unresolved.map(_.task)
+    unresolved.map(u => resolveDependencies(u, allTasks))
+  }
+
+  private def toUnresolvedTask(discoveredTask: DiscoveredTask): UnresolvedTask =
+    UnresolvedTask(
+      task = Task(
+        id = java.util.UUID.randomUUID(),
+        name = discoveredTask.name,
+        command = discoveredTask.command,
+        description = discoveredTask.description,
+        dependencies = List.empty,
+        source = discoveredTask.source
+      ),
+      dependencyNames = discoveredTask.dependencies
+    )
+
+  private def resolveDependencies(
+      unresolved: UnresolvedTask,
+      allTasks: List[Task]
+  ): Task = {
+    val deps = unresolved.task.source match {
+      case Npm  => resolveNpmDeps(unresolved.task, allTasks)
+      case Yaml => resolveYamlDeps(unresolved, allTasks)
+    }
+    unresolved.task.copy(dependencies = deps)
+  }
+
+  private def resolveNpmDeps(task: Task, allTasks: List[Task]): List[Task] =
+    allTasks
+      .filter(other => other.name != task.name)
+      .filter(other => other.dependencies.contains(task.name))
+
+  private def resolveYamlDeps(
+      unresolved: UnresolvedTask,
+      allTasks: List[Task]
+  ): List[Task] =
+    allTasks.filter(other => unresolved.dependencyNames.contains(other.name))
+}
