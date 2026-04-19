f8f834c72669d86c52202732897c36e54312c568
aaron <aaronwan808@gmail.com>
2026-04-19 20:40:01 +0100

    Working on task executor

--- src/main/scala/executor/TaskExecutor.scala
+++ src/main/scala/executor/TaskExecutor.scala
@@ -1,6 +1,9 @@
 package executor
 
+import cats.effect.IO
 import core.Task
+import fs2.io.process.{ProcessBuilder, Processes}
+import fs2.text
 
 /** This class accepts a sorted list of tasks from the TaskGraphResolver which then essentially
   * executes these tasks in an optimal way by executing tasks that have no dependencies in parallel
@@ -8,7 +11,31 @@ import core.Task
   */
 object TaskExecutor {
 
-  def executeTasks() = ???
+  def execute(sortedTasks: List[Task]) = {
+    val batches = compileTaskBatch(sortedTasks)
+    executeTaskBatch(batches)
+  }
+
+  private def executeTaskBatch(taskBatch: List[Task]): List[IO[Unit]] = {
+    val batches = this.compileTaskBatch(taskBatch)
+    batches.flatMap(batch => batch.map(task => runTask(task)))
+  }
+
+  private def runTask(task: Task): IO[Unit] = {
+    ProcessBuilder("sh", List("-c", task.command))
+      .withInheritEnv(true)
+      .withWorkingDirectory(task.path)
+      .spawn[IO]
+      .use(process => {
+        process.stdout
+          .through(text.utf8.decode)
+          .through(text.lines)
+          .evalMap(line => IO.println(s"[${task.name}] $line"))
+          .compile
+          .drain
+      })
+
+  }
 
   /** This function is used to create a list of lists where each inner list represents a batch of
     * tasks to execute together. We create this by reducing on the given sortedList. The starting value
@@ -33,5 +60,4 @@ object TaskExecutor {
     }
   }
 
-  
 }
