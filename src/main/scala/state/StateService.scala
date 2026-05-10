6c5293649cb3936e209a7d03fbf000a0e4d6cd69
aaron <aaronwan808@gmail.com>
2026-05-11 00:28:00 +0100

    updateSingleTaskState

--- src/main/scala/state/StateService.scala
+++ src/main/scala/state/StateService.scala
@@ -25,4 +25,18 @@ class StateService[F[_]: Monad](ref: Ref[F, SystemState]) {
       state.taskStatuses.filter { case (key, value) => key.id == taskId }
     }
   }
+
+  def updateSingleTaskState(
+      taskId: UUID,
+      taskStatus: TaskStatus
+  ): F[Boolean] = {
+    // takes a function that returns the new state and also funcition return
+    ref.modify { state =>
+      state.taskStatuses.find { case (key, value) => key.id == taskId } match {
+        case Some((task, _)) =>
+          (state.taskStatuses.update(task, taskStatus), true)
+        case None => (state.taskStatuses, false)
+      }
+    }
+  }
 }
