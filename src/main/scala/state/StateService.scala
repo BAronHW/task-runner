8228f119de81266f8f68c5f3a7546af95277a6cf
aaron <aaronwan808@gmail.com>
2026-05-10 23:23:44 +0100

    writign the StateService.scala

--- src/main/scala/state/StateService.scala
+++ src/main/scala/state/StateService.scala
@@ -1,12 +1,28 @@
 package state
 
+import cats.Monad
 import cats.effect.{IO, Ref}
+import core.{SystemState, Task, TaskStatus}
+import cats.implicits._
+
+import java.util.UUID
 
 /** This is a service that exposes an interface for modifying the system state so that the user can
   * understand what tasks have been run and which ones have not been run yet
   */
-object StateService {
-  val ref = Ref[IO]
+class StateService[F[_]: Monad](ref: Ref[F, SystemState]) {
 
-  def setState(): IO[Unit] = {}
+  def setState(state: SystemState): F[Unit] = {
+    ref.set(state)
+  }
+
+  def getAllState(): F[SystemState] = {
+    ref.get
+  }
+
+  def getSingleTaskState(taskId: UUID): F[Map[Task, TaskStatus]] = {
+    ref.get.map { state =>
+      state.taskStatuses.filter { case (key, value) => key.id == taskId }
+    }
+  }
 }
